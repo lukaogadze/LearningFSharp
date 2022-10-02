b2bd2c9516138ba7344c1d19a737be1648a86262
lukaogadze <lukaogadze@live.com>
2022-10-02 17:35:55 +0400

    initial | added first example

---
+++ WhatIsFSharp/ChapterOne/ExampleOne.fs
@@ -0,0 +1,10 @@
+﻿open System // Get access to functionality in System namespace
+
+// Define a list of names
+let names = ["Peter"; "Julia"; "Xi"]
+
+// Define a function that takes a name and produces a greeting
+let getGreeting name = $"Hello {name}"
+
+// Print a greeting for each name
+names |> List.map getGreeting |> List.iter (fun greeting -> printfn $"{greeting}! Enjoy your F#")

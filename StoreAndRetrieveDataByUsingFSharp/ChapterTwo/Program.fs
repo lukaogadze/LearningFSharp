094ac5b348af7877f1fa84b66c9e4871e13b04fd
lukaogadze <lukaogadze@live.com>
2022-10-09 09:48:47 +0400

    End of Store and retrieve data by using F#

---
+++ StoreAndRetrieveDataByUsingFSharp/ChapterTwo/Program.fs
@@ -0,0 +1,15 @@
+﻿open type System.Console
+printfn "Welcome to the calculator program"
+// // read input from the console and assign to `sum`
+// let sum = 0
+// printfn $"The sum is %i{sum}"
+
+printfn "Type the first number"
+let firstNumber = ReadLine() |> int
+printfn "Type the second number"
+let secondNumber = ReadLine() |> int
+printfn $"First %i{firstNumber}, Second %i{secondNumber}"
+
+let sum = firstNumber + secondNumber
+
+printfn $"The sum is %i{sum}"

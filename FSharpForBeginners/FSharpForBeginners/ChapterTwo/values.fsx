163e9fb1c615a063d76fdb5baf8f054205897887
lukaogadze <lukaogadze@live.com>
2022-10-10 06:59:47 +0400

    i have go to work commit :D

---
+++ FSharpForBeginners/FSharpForBeginners/ChapterTwo/values.fsx
@@ -0,0 +1,32 @@
+"New York"
+
+true
+
+13
+
+(1.0, 3.0)
+
+["Learn F#"; "Build app"; "Profit"]
+
+fun input -> input / 3
+
+let cityName = "New York"
+
+let fsharpIsAwesome = true
+
+let luckyNumber = 13
+
+let coordinates = (1.0, 3.0)
+
+let toDoList = ["Learn F#"; "Build app"; "Profit"]
+
+let divideByThree = fun input -> input / 3
+
+let luckyNumberString: string = "13"
+
+// cityName <- "New York"
+
+let wrongName = "New yok"
+let mutable mutableCityName = "new york"
+
+mutableCityName <- wrongName

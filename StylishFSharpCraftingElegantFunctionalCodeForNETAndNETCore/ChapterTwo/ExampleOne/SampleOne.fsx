3e250f4dbfd85abdfd9af2a9f5ecfa82267a81e5
lukaogadze <lukaogadze@live.com>
2022-10-15 11:59:55 +0400

    break commit

---
+++ StylishFSharpCraftingElegantFunctionalCodeForNETAndNETCore/ChapterTwo/ExampleOne/SampleOne.fsx
@@ -0,0 +1,9 @@
+﻿open System
+
+let convertMilesYards (milesPointYards: float): float =
+    let wholeMiles = milesPointYards |> floor
+    let fractions = milesPointYards - float wholeMiles
+    wholeMiles + (fractions / 0.1760)
+    
+
+let decimalMiles = 1.0880 |> convertMilesYards
